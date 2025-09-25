99166fffdb5b4780e1989e8acdf34ac3a7d31972
aaronkao88 <aaronkao88@gmail.com>
2025-09-25 17:23:57 +0800

    add: create about api

--- src/app/_components/AboutsText/pages.js
+++ src/app/_components/AboutsText/pages.js
@@ -1,25 +1,47 @@
 "use client"; 
-import { motion } from "motion/react"
+import { useEffect, useState } from "react";
+import { motion } from "motion/react";
 
 export default function AboutsText() {
-  const AboutText = {
-    text: "我是一名充滿熱忱的後端開發人員，對資訊技術有高度興趣。在實習期間，我在一家新創公司學習並實踐了後端技術（如 Node.js）和前端框架（如 Vue.js）。這段經歷讓我參與了多個專案開發，並與資深工程師合作，提升了技術能力與解決問題的技巧。",
-    linkRel: "Abouts",
-    linkHref: "https://i.meee.com.tw/2YdNQpU.png",
-    linkAction: "GET",
-  };
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    async function fetchAbouts() {
+      try {
+        const res = await fetch("/api/about");
+        const result = await res.json();    
+        const aboutsArray = result?.data?.getAbouts || [];
+        setAbouts(aboutsArray);
+      } catch (err) {
+        console.error("載入關於我的資料錯誤：", err);
+        setAbouts([]);
+      }
+    }
+
+    fetchAbouts();
+  }, []);
 
   return (
-    <motion.section initial={{ opacity: 0, y: 50 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true, amount: 0.5 }} transition={{ duration: 0.6, ease: "easeOut" }} 
-     className="lg:w-full h-screen flex flex-col md:flex-row items-center justify-center gap-10 mt-3">
-        <div className="col">
-            <h2 className="text-3xl font-bold mb-4 text-[50px]">Abouts</h2>
-            <hr className="relative h-px my-8 bg-white border-0 mt-5 md:hidden sm:hidden" />
-            <p className="text-[15px]">關於 Aaron</p>
-        </div>
-        <div className="col">
-            <p className="relative w-md mt-5 sm:w-sm">{AboutText.text}</p>
-        </div>
+    <motion.section
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true, amount: 0.5 }}
+      transition={{ duration: 0.6, ease: "easeOut" }}
+      className="lg:w-full h-screen flex flex-col md:flex-row items-center justify-center gap-10 mt-3"
+    >
+      <div className="col">
+        <h2 className="text-3xl font-bold mb-4 text-[50px]">Abouts</h2>
+        <hr className="lg:block relative h-px my-8 bg-white border-0 mt-5 md:hidden sm:hidden" />
+        <p className="text-[15px]">關於 Aaron</p>
+      </div>
+    
+     {abouts.map((about, index) => (
+      <div className="col" key={index}>
+        <p className="relative w-md mt-5 sm:w-sm">
+          {about.text}
+        </p>
+      </div>
+    ))}
     </motion.section>
   );
 }
