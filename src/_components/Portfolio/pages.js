e2ba5723660a183c87cbbd038d2ba8e22c9fc50e
aaronkao88 <aaronkao88@gmail.com>
2025-09-25 11:26:14 +0800

    back: css style setting

--- src/_components/Portfolio/pages.js
+++ src/_components/Portfolio/pages.js
@@ -13,7 +13,7 @@ export default function Experience() {
   }
 
   return (
-    <motion.section initial={{ opacity: 0, y: 50 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true, amount: 0.5 }} transition={{ duration: 0.6, ease: "easeOut" }} className="lg:w-full h-screen relative flex flex-col md:flex-row sm:flex-row justify-center items-center gap-36 p-10">
+    <motion.section initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} className="lg:w-full h-screen relative flex flex-col md:flex-row sm:flex-row justify-center items-center gap-36 p-10">
       <div className="flex flex-col">
         <h2 className="text-[50px] font-bold mb-4">Portfolio</h2>
         <hr className="h-px my-4 bg-white border-0" />
@@ -22,7 +22,7 @@ export default function Experience() {
 
       <div className="flex flex-col gap-8">
         <div className="relative flex flex-col">
-          <div className="w-[150px] h-[50px bg-gray-200 rounded-lg flex flex-row items-center justify-center gap-5 top-5 md:top-0 relative">
+          <div className="w-[150px] h-[50px]  bg-gray-200 rounded-lg flex flex-row items-center justify-center gap-5 top-5 md:top-0 relative">
                 <button className="bg-blue-700 w-[60px] h-10 rounded-md hover:bg-blue-600 transition-colors">Pinned</button>
                 <button className="w-15 h-10 rounded-md bg-transparent text-black hover:bg-gray-500 hover:text-white transition-colors">All</button>
           </div> 
